55cebbaa47d925de6822c4bf03012ca59b3b2a3d
Sangsu Baek <skygana@gmail.com>
2021-05-08 23:24:21 +0900

    username -> email로 변경

--- src/main/java/com/cafe/pjt/service/UserService.java
+++ src/main/java/com/cafe/pjt/service/UserService.java
@@ -1,13 +1,14 @@
 package com.cafe.pjt.service;
 
-import com.cafe.pjt.repository.model.User;
+import com.cafe.pjt.vo.User;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
     void join(User user);
-    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String username);
     List<User> findAll();
+    String encodePassword(String password);
     boolean matchPassword(String rawPassword, String encodedPassword);
 }
